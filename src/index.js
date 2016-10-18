83da199c317e346e55e58083655e1d5180930a68
khai96_ <hvksmr1996@gmail.com>
2016-10-18 23:20:21 +0700

    Load URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const {join} = require('path')
 const {app, BrowserWindow} = require('electron')
+const location = join(__dirname, 'page', 'index.xml')
 
 app.on('ready', createWindow)
 app.on('activate', () => win || createWindow())
@@ -13,5 +15,6 @@ function createWindow () {
   })
   win.on('closed', () => { win = null })
   win.on('ready-to-show', () => win.show())
+  win.loadURL(location)
   win.focus()
 }
