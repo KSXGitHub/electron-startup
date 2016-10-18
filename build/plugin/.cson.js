102443e564fa84c2ef6fea61d846f61c7cf5fda8
khai96_ <hvksmr1996@gmail.com>
2016-10-18 23:06:45 +0700

    Always end with newline

--- build/plugin/.cson.js
+++ build/plugin/.cson.js
@@ -2,5 +2,5 @@
 const {eval} = require('coffee-script')
 const {stringify} = JSON
 const ext = '.json'
-const compile = buffer => ({buffer: stringify(eval(buffer.toString('utf8'))), ext})
+const compile = buffer => ({buffer: stringify(eval(buffer.toString('utf8'))) + '\n', ext})
 module.exports = compile
--- build/plugin/.pug.js
+++ build/plugin/.pug.js
@@ -4,6 +4,6 @@ const pug = require('pug')
 const ext = '.xml'
 const compile = buffer => {
   const create = pug.compile(buffer.toString('utf8'), {pretty, doctype: 'xml'})
-  return {buffer: create(), ext}
+  return {buffer: create() + '\n', ext}
 }
 module.exports = compile
