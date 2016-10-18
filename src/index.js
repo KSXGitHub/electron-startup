a5ea296a3bb8be495c44446e1ae9324465cef007
khai96_ <hvksmr1996@gmail.com>
2016-10-18 23:17:37 +0700

    Complete electron launcher

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 'use strict'
 
-const {app} = require('electron')
+const {app, BrowserWindow} = require('electron')
 
-app.once('ready', main)
+app.on('ready', createWindow)
+app.on('activate', () => win || createWindow())
+app.on('windows-all-closed', () => app.quit())
 
-function main () {
-  app.quit()
+let win = null
+function createWindow () {
+  win = new BrowserWindow({
+    autoHideMenuBar: true
+  })
+  win.on('closed', () => win = null)
+  win.on('ready-to-show', () => win.show())
+  win.focus()
 }
